bb7c764acedbfb4f393e80dd2449fbee9492ade0
bryanlmk <mrpanda100@gmail.com>
2017-06-02 15:58:46 +0800

    Initial commit

---
+++ FragmentSecond.java
@@ -0,0 +1,81 @@
+package com.myapplicationdev.android.smsretriever;
+
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.PermissionChecker;
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentSecond extends Fragment {
+
+    Button btnAddText1;
+    TextView tvFrag2;
+
+    public FragmentSecond() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_second,container,false);
+
+        tvFrag2 = (TextView) view.findViewById(R.id.tvFrag2);
+        btnAddText1 = (Button) view.findViewById(R.id.btnAddTextFrag2);
+
+        btnAddText1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int permissionCheck = PermissionChecker.checkSelfPermission(getActivity(), Manifest.permission.READ_SMS);
+                if(permissionCheck !=PermissionChecker.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.READ_SMS},0);
+                    return;
+                }
+                Uri uri = Uri.parse("content://sms");
+                String[] reqCols = new String[]{"date","address","body","type"};
+
+                ContentResolver cr = getActivity().getContentResolver();
+
+                String filter = "body LIKE ? AND body LIKE ?";
+
+                String[] filterArgs = {"%late%","%min%"};
+
+                Cursor cursor = cr.query(uri,reqCols,null,null,null);
+                String smsBody = "";
+                if (cursor.moveToFirst()) {
+                    do {
+                        long dateInMillis = cursor.getLong(0);
+                        String date = (String) DateFormat
+                                .format("dd MMM yyyy h:mm:ss aa", dateInMillis);
+                        String address = cursor.getString(1);
+                        String body = cursor.getString(2);
+                        String type = cursor.getString(3);
+                        if (type.equalsIgnoreCase("1")) {
+                            type = "Inbox:";
+                        } else {
+                            type = "Sent:";
+                        }
+                        smsBody += type + " " + address + "\n at " + date
+                                + "\n\"" + body + "\"\n\n";
+                    } while (cursor.moveToNext());
+                }
+                tvFrag2.setText(smsBody);
+            }
+        });
+        return view;
+    }
+
+}
